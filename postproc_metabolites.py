fafd6769c68797aaf2cebaaaf8e249b9d620a87b
Asbjørn Bjørkkjær <asbjorn.bjorkkjar@ntnu.no>
2025-04-10 14:58:28 +0200

    Working single metabolits pipeline

--- postproc_metabolites.py
+++ postproc_metabolites.py
@@ -24,7 +24,7 @@ def ppval(p, x):
             result = result * x + p[i]
         return result
 
-def EVAL_GluCEST(data_path, seq_path, date):
+def EVAL_GluCEST(data_path, seq_path):
     import pypulseq as pp # the import over is not found
     seq = pp.Sequence()
     print('--- Reading the sequence protocol ---')
@@ -95,7 +95,7 @@ def EVAL_GluCEST(data_path, seq_path, date):
             V.shape[3], V.shape[0], V.shape[1], V.shape[2]
         ).transpose(1, 2, 3, 0)
 
-    print('--- Plotting ---')
+    print('--- Plotting GluCEST images ---')
     slice_of_interest = 0 # pick slice for evaluation (0 if only one slice)
     desired_offset = 3 # pick offset for evaluation (3 for GluCEST at 3 ppm)
     offset_of_interest = np.where(offsets == desired_offset)[0]  
@@ -117,131 +117,89 @@ def EVAL_GluCEST(data_path, seq_path, date):
     plt.show()
 
     # Choose pixels for ROI
-    if date == '250312':
-        pixels_0mm = [66,71,80,85] # 250312
-        pixels_2mm = [81,86,67,72] # 250312
-        pixels_4mm = [76,81,47,52] # 250312
-        pixels_6mm = [57,62,41,46] # 250312
-        pixels_8mm = [43,48,54,59] # 250312
-        pixels_10mm = [47,52,74,79] # 250312
-    elif date == '250313':
-        pixels_0mm = [66,71,80,85] # 250313
-        pixels_2mm = [81,86,67,72] # 250313
-        pixels_4mm = [76,81,47,52] # 250313
-        pixels_6mm = [57,62,41,46] # 250313
-        pixels_8mm = [43,48,54,59] # 250313
-        pixels_10mm = [47,52,74,79] # 250313
-    elif date == '250317':
-        pixels_0mm = [62,67,81,86] # 250317
-        pixels_2mm = [77,82,68,73] # 250317
-        pixels_4mm = [72,77,49,54] # 250317
-        pixels_6mm = [54,59,42,47] # 250317
-        pixels_8mm = [40,45,56,61] # 250317
-        pixels_10mm = [43,48,75,80] # 250317
-    elif date == '250324':
-        pixels_0mm = [40,45,56,61] # 250324
-        pixels_2mm = [44,49,75,80] # 250324
-        pixels_4mm = [62,67,81,86] # 250324
-        pixels_6mm = [77,82,67,72] # 250324
-        pixels_8mm = [72,77,48,53] # 250324
-        pixels_10mm = [54,59,42,47] # 250324
-    elif date == '250407':
-        pixels_0mm = [72,77,44,49] # 250407
-        pixels_2mm = [53,58,44,49] # 250407
-        pixels_4mm = [44,49,61,66] # 250407
-        pixels_6mm = [54,59,78,83] # 250407
-        pixels_8mm = [74,79,77,82] # 250407
-        pixels_10mm = [83,87,60,65] # 250407
-    elif date == '250408_PBScheck':
-        pixels_0mm = [0,2,0,2] # nan
-        pixels_2mm = [0,2,0,2] # nan
-        pixels_4mm = [0,2,0,2] # nan
-        pixels_6mm = [56,66,50,60] # PBS water
-        pixels_8mm = [37,46,64,74] # 250408 - one of 10mM
-        pixels_10mm = [55,64,73,82] # 250408 - one of 10mM
-    elif date == '250408':
-        pixels_0mm = [0,2,0,2] # nan
-        pixels_2mm = [0,2,0,2] # nan
-        pixels_4mm = [0,2,0,2] # nan
-        pixels_6mm = [56,66,50,60] #
-        pixels_8mm = [37,46,64,74] # 
-        pixels_10mm = [79,84,52,57] # 250408 
-    elif date == '250409':
-        pixels_0mm = [0,2,0,2] # 250409
-        pixels_2mm = [0,2,0,2] # 250409
-        pixels_4mm = [0,2,0,2] # 250409
-        pixels_6mm = [0,2,0,2] # 250409
-        pixels_8mm = [0,2,0,2] # 250409
-        pixels_10mm = [45,50,51,56] # 250409
+    pixels_dict = { 
+        'glu': [45,50,51,56],     # 250409
+        'gln': [43,48,70,75],     # 250409
+        'gaba': [59,64,82,87],    # 250409
+        'naa': [77,82,73,78],     # 250409
+        'cr': [79,84,54,59],      # 250409
+        'taurine': [63,68,43,48], # 250409
+    }
 
+    # Choose metabolites
+    label_names = ['Glu', 'Gln', 'GABA', 'NAA', 'Cr', 'Taurine']
+    m_avg = []
+    m_sem = []
 
-    # Spectrum handling phantom
-    array_Z = V_Z_corr_reshaped[pixels_10mm[0]:pixels_10mm[1],pixels_10mm[2]:pixels_10mm[3],0,1:]
-    flattened_vectors_Z = array_Z.reshape(-1, array_Z.shape[-1]) 
-    Z_spectrum = flattened_vectors_Z.mean(axis=0)
+    print('--- Plotting GluCEST spectra ---')
+    plt.figure(figsize=(12, 4))
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(label_names)))
 
-    V_MTRasym_reshaped_pc = V_MTRasym_reshaped*100
-    array_MTR = V_MTRasym_reshaped_pc[pixels_10mm[0]:pixels_10mm[1],pixels_10mm[2]:pixels_10mm[3],0,1:]
-    flattened_vectors_MTR = array_MTR.reshape(-1, array_MTR.shape[-1]) 
-    MTR_spectrum = flattened_vectors_MTR.mean(axis=0)
-    
-    plt.figure(figsize=(10, 4))
-    plt.subplot(1, 2, 1)
-    plt.plot(w, Z_spectrum, "r.-")
-    plt.gca().invert_xaxis()
-    plt.title("Mean Z-spectrum in 10 mM")
-    
-    plt.subplot(1, 2, 2)
-    plt.plot(w, MTR_spectrum, "b.-")
-    plt.xlim([0, 4])
-    plt.gca().invert_xaxis()
-    plt.title("Mean MTRasym-spectrum in 10 mM")
+    for i, label in enumerate(label_names):
+        key = label.lower()
+        pixels_metabolite = pixels_dict.get(key)
+
+        # Spectrum handling phantom
+        array_Z = V_Z_corr_reshaped[pixels_metabolite[0]:pixels_metabolite[1],pixels_metabolite[2]:pixels_metabolite[3],slice_of_interest,1:] # 1: to remove the M0 scan
+        flattened_vectors_Z = array_Z.reshape(-1, array_Z.shape[-1]) 
+        Z_spectrum = flattened_vectors_Z.mean(axis=0)
+
+        V_MTRasym_reshaped_pc = V_MTRasym_reshaped*100
+        array_MTR = V_MTRasym_reshaped_pc[pixels_metabolite[0]:pixels_metabolite[1],pixels_metabolite[2]:pixels_metabolite[3],slice_of_interest,1:] # 1: to remove the M0 scan
+        flattened_vectors_MTR = array_MTR.reshape(-1, array_MTR.shape[-1]) 
+        MTR_spectrum = flattened_vectors_MTR.mean(axis=0)
+
+        # Get statistics
+        m_roi = V_MTRasym_reshaped_pc[pixels_metabolite[0]:pixels_metabolite[1],pixels_metabolite[2]:pixels_metabolite[3], slice_of_interest, offset_of_interest]
+        avg, sem = np.mean(m_roi.reshape(-1)), sc.stats.sem(m_roi.reshape(-1))
+        m_avg.append(avg)
+        m_sem.append(sem)
+
+        plt.subplot(1, 2, 1)
+        plt.plot(w, Z_spectrum, marker='o', markersize=2, label=label_names[i], color=colors[i])
+        plt.axvline(x=3, color='grey', linestyle='--', linewidth=0.8, alpha=0.7)
+        plt.xlim([-5, 5])
+        plt.ylim([0.12,1.1])
+        plt.xlabel('Frequency offset [ppm]')
+        plt.ylabel('Normalized MTR')
+        plt.gca().invert_xaxis()
+        plt.title("Z-spectra")
+        plt.legend()
+
+        plt.subplot(1, 2, 2)
+        plt.plot(w, MTR_spectrum, marker='o', markersize=2, label=label_names[i], color=colors[i])
+        plt.axvline(x=3, color='grey', linestyle='--', linewidth=0.8, alpha=0.7)
+        plt.xlim([0, 4])
+        plt.ylim([-0.05,30])
+        plt.xlabel('Frequency offset [ppm]')
+        plt.ylabel('MTRasym [%]')
+        plt.gca().invert_xaxis()
+        plt.title("MTRasym-spectra")
+        plt.legend()
+
+    plt.plot
     plt.show()
 
     # GluCEST effect for each [Glu]
-    mm0 = V_MTRasym_reshaped_pc[pixels_0mm[0]:pixels_0mm[1],pixels_0mm[2]:pixels_0mm[3], slice_of_interest, offset_of_interest]
-    mm0_avg, mm0_sem = np.mean(mm0.reshape(-1)), sc.stats.sem(mm0.reshape(-1))
-  
-    mm2 = V_MTRasym_reshaped_pc[pixels_2mm[0]:pixels_2mm[1],pixels_2mm[2]:pixels_2mm[3], slice_of_interest, offset_of_interest]
-    mm2_avg, mm2_sem = np.mean(mm2.reshape(-1)), sc.stats.sem(mm2.reshape(-1))
-   
-    mm4 = V_MTRasym_reshaped_pc[pixels_4mm[0]:pixels_4mm[1],pixels_4mm[2]:pixels_4mm[3], slice_of_interest, offset_of_interest]
-    mm4_avg, mm4_sem = np.mean(mm4.reshape(-1)), sc.stats.sem(mm4.reshape(-1))
-
-    mm6 = V_MTRasym_reshaped_pc[pixels_6mm[0]:pixels_6mm[1],pixels_6mm[2]:pixels_6mm[3], slice_of_interest, offset_of_interest]
-    mm6_avg, mm6_sem = np.mean(mm6.reshape(-1)), sc.stats.sem(mm6.reshape(-1))
-
-    mm8 = V_MTRasym_reshaped_pc[pixels_8mm[0]:pixels_8mm[1],pixels_8mm[2]:pixels_8mm[3], slice_of_interest, offset_of_interest]
-    mm8_avg, mm8_sem = np.mean(mm8.reshape(-1)), sc.stats.sem(mm8.reshape(-1))
-
-    pixels_10mm = pixels_10mm
-    mm10 = V_MTRasym_reshaped_pc[pixels_10mm[0]:pixels_10mm[1],pixels_10mm[2]:pixels_10mm[3], slice_of_interest, offset_of_interest]
-    mm10_avg, mm10_sem = np.mean(mm10.reshape(-1)), sc.stats.sem(mm10.reshape(-1))
-
-    mm = np.array([0,2,4,6,8,10])
-    mm_avg = np.array([mm0_avg, mm2_avg, mm4_avg, mm6_avg, mm8_avg, mm10_avg])
-    mm_sem = np.array([mm0_sem, mm2_sem, mm4_sem, mm6_sem, mm8_sem, mm10_sem])
+    metabolites = np.arange(len(label_names))
+    m_avg = np.array(m_avg)
+    m_sem = np.array(m_sem)
 
     # Plot data with error bars
-    plt.errorbar(mm, mm_avg, yerr=mm_sem, fmt='o', label="Averages of data with SEM", capsize=6)
-
-    # Fit a linear trend line
-    slope, intercept = np.polyfit(mm, mm_avg, 1)  
-    trend_line = slope * mm + intercept  
-
-    plt.plot(mm, trend_line, 'r--', label=f"Trend: y={slope:.2f}x + {intercept:.2f}")
-    plt.xlabel("Concentration of Glu [mM]")
+    print('--- Plotting GluCEST effect ---')
+    plt.errorbar(metabolites, m_avg, yerr=m_sem, fmt='o', label="Average ± SEM", capsize=6)
+    plt.xlabel("Metabolites")
     plt.ylabel("MTRasym contrast [%]")
-    plt.title("Linear trend with concentrations")
+    plt.title("GluCEST effect for metabolites")
+    plt.xticks(metabolites, label_names)
     plt.legend()
-    '''plt.grid(True)'''
+    plt.grid(True, which='both', linestyle='--', linewidth=0.3, color='lightgrey', alpha=0.7)
     plt.show()
     
 
 if __name__ == "__main__":
     globals()["EVAL_GluCEST"] = EVAL_GluCEST 
     EVAL_GluCEST(
-        data_path=r'C:\asb\ntnu\MRIscans\250409\dicoms\E4', 
-        seq_path=r'C:\asb\ntnu\MRIscans\250409\seq_files\seq_file_E4.seq',
-        date = '250409'
+        data_path=r'C:\asb\ntnu\MRIscans\250409\dicoms\E10', 
+        seq_path=r'C:\asb\ntnu\MRIscans\250409\seq_files\seq_file_E10.seq',
     )
